72767f68dde50bbc1eb5492eb8fafcedf6262bc2
Chris Doan <cdoan@redhat.com>
2020-07-19 21:12:06 -0500

    add check for pods

--- start.sh
+++ start.sh
@@ -5,3 +5,9 @@ source utils.sh
 printf "Setup connect to OpenShift Cluster"
 oc login -s $OCP_API -u $OCP_USERNAME -p $OCP_PASSWORD --insecure-skip-tls-verify=true
 oc cluster-info
+
+NS=$(NS:-open-cluster-management}
+
+if oc get namespace | grep $NS ; then
+  oc get pods -n $NS
+fi
