65f0dc626ed4f8734611797002b43ac6cf1666a2
Chris Doan <cdoan@redhat.com>
2020-07-19 21:24:59 -0500

    fix variable

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ printf "Setup connect to OpenShift Cluster"
 oc login -s $OCP_API -u $OCP_USERNAME -p $OCP_PASSWORD --insecure-skip-tls-verify=true
 oc cluster-info
 
-NS=$(NS:-open-cluster-management}
+NS=${NS:-open-cluster-management}
 
 if oc get namespace | grep $NS ; then
   oc get pods -n $NS
